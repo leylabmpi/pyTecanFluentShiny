8c95770977b6aad075cc65a851f1fc2102641e9c
Nick Youngblut <nyoungb2@gmail.com>
2017-09-07 09:05:51 +0200

    updated script path

--- pool/server.R
+++ pool/server.R
@@ -110,10 +110,10 @@ load_ex_map_file = function(){
 shinyServer(function(input, output, session) {
   # script path & subcommand
   #script_path = '/Users/nyoungblut/anaconda2/bin/pyTecanFluent'
-  script_path = '/Users/nyoungblut/anaconda2/envs/py35/bin/pyTecanFluent'
+  #script_path = '/Users/nyoungblut/anaconda2/envs/py35/bin/pyTecanFluent'
   #script_path = '/Users/nick/anaconda3/envs/py3_dev/bin/pyTecanFluent'
   #script_path = '/usr/local/bin/pyTecanFluent'
-  #script_path = '/home/shiny/miniconda3/envs/py3/bin/pyTecanFluent'
+  script_path = '/home/shiny/miniconda3/envs/py3/bin/pyTecanFluent'
   subcommand = 'pool'
   
   # calling script
