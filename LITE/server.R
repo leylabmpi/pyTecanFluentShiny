cd248a20e25e711f9d25d457d6695f3da555d1a7
Nick Youngblut <nyoungb2@gmail.com>
2018-03-06 12:23:17 +0100

    added LITE

---
+++ LITE/server.R
@@ -0,0 +1,111 @@
+# Shiny server
+library(shiny)
+library(readxl)
+source("../utils/io.R")
+source("../utils/format.R")
+
+
+
+#' calling 'LITE' subcommand
+call_LITE = function(script_path, subcommand,input){
+  # command
+  if(is.null(input$MapFile)){
+    options = c('-h')
+  } else {
+    # dealing with --prefix
+    prefix = file.path(tempdir(), gsub('^.+/', '', input$prefix))
+    
+    # compiling options
+    options = c(
+      # I/O
+      rename_tmp_file(input$MapFile),
+      c('--rows', add_quotes(input$rows)),
+      c('--prefix', add_quotes(prefix)),
+      # Destination plate
+      c('--dest-name', add_quotes(input$destname)),
+      c('--dest-type', add_quotes(input$desttype)),
+      c('--dest-start', input$deststart),
+      # Reagents
+      c('--tag-mm-volume', input$tag_mm_volume),
+      c('--pcr-mm-volume', input$pcr_mm_volume),
+      c('--sample-volume', input$sample_volume),
+      c('--primer-volume', input$primer_volume),
+      c('--error-perc', add_quotes(input$errorperc)),
+      # Liquid classes
+      c('--mm-liq', add_quotes(input$mm_liq)),
+      c('--sample-liq', add_quotes(input$sample_liq)),
+      c('--primer-liq', add_quotes(input$primer_liq)),
+      c('--n-tip-reuse', add_quotes(input$n_tip_reuse)),
+      c('--n-multi-disp', add_quotes(input$n_multi_disp))
+    ) 
+  }
+  # call with options
+  options = paste(c(subcommand, options), collapse=' ')
+  system2(script_path, options, stdout=TRUE, stderr=TRUE)
+}
+
+#' get files created by the command
+get_files_created = function(x){
+  x = gsub('File written: +', '', x)
+  x[sapply(x, file.exists)]
+}
+
+#' loading example mapping file
+load_ex_map_file = function(){
+  df = read.delim('../data/LITE_96well.txt', sep='\t')
+  colnames(df)[1] = '#SampleID'
+  return(df)
+}
+
+
+#-- server --#
+shinyServer(function(input, output, session) {
+  # script path & subcommand
+  #script_path = '/Users/nyoungblut/anaconda2/bin/pyTecanFluent'
+  #script_path = '/Users/nick/anaconda3/envs/py3_dev/bin/pyTecanFluent'
+  #script_path = '/usr/local/bin/pyTecanFluent'
+  script_path = '/home/shiny/miniconda3/envs/py3/bin/pyTecanFluent'
+  subcommand = 'LITE'
+  
+  # calling script
+  script_out = eventReactive(input$runBtn, {
+    # run command 
+    call_LITE(script_path, subcommand, input)
+  })
+  
+  # adding script output to output
+  output$script_out = reactive({
+    ret = gsub('File written:.+/', 'File created: ', script_out())
+    paste(ret, collapse='\n')
+  })
+  
+  # number of output files
+  output$download_btn = renderUI({
+    n_files = length(grep('^File written: ', script_out())) 
+    if(n_files > 0){
+      downloadButton('downloadData', 'Download results')
+    }
+  })
+  
+  # file download
+  output$downloadData = downloadHandler(
+    filename = function() { paste0(input$prefix, '.zip') },
+    content = function(file_name) {
+      files = get_files_created(script_out())
+      zip(file_name, files, flags='-r9Xj')
+    }
+  )
+  
+  # example data table
+  output$example_tbl = DT::renderDataTable(
+    load_ex_map_file(),
+    extensions = c('Buttons'),
+    rownames = FALSE,
+    options = list(
+      pageLength = 40,
+      dom = 'Brt',
+      buttons = c('copy', 'csv', 'excel', 'pdf', 'print')
+    )
+  )
+})
+
---
+++ LITE/ui.R
@@ -0,0 +1,188 @@
+# Shiny UI
+library(shiny)
+
+#-- UI --#
+shinyUI(fluidPage(
+  
+  titlePanel("Map2Robot"),
+  fluidRow(
+    column(6,
+      h5('Convert a QIIME-formatted mapping file into a Tecan worklist file for LITE (hacked Nextera) library prep')
+    )
+  ),
+  fluidRow(
+    verbatimTextOutput('script_out')
+  ),
+  fluidRow(
+    column(2,
+      br(),
+      actionButton("runBtn", "Run command"),
+      br(),
+      br()
+    ),
+    column(2,
+      br(),
+      uiOutput('download_btn'),
+      br(),
+      br()
+    )
+  ),
+  tabsetPanel(
+    tabPanel("Description", 
+      fluidRow(
+        column(12,
+               h5('Convert a QIIME-formatted mapping file to a GWL file, which is used by the TECAN robot to conduct LITE (hacked Nextera) library prep (ie., combining MasterMix, primers, samples, etc)'),
+               h5('The mapping file must contain some extra columns that will tell the robot where the samples are.'),
+               h3('Input'),
+               h4('Extra columns needed in the mapping file:'),
+               h6(br('If no barcoding, single barcoding, or dual barcodes already combined:')),
+               h5('"TECAN_sample_labware_name"'),
+               h6('The sample labware name on the robot worktable'),
+               h5('"TECAN_sample_labware_type"'),
+               h6('The type of labware containing samples (eg., "PCR Adapter 96 Well and 96 Well Eppendorf TwinTec PCR")'),
+               h5('"TECAN_sample_target_position"'),
+               h6('The well or tube location (a number)'),
+               h5('"TECAN_primer_labware_name"'),
+               h6('The primer plate labware name on the robot worktable (eg., "515F-806R")'),
+               h5('"TECAN_primer_labware_type"'),
+               h6('The primer plate labware type on the robot worktable (eg., "96 Well Eppendorf TwinTec PCR"'),
+               h5('"TECAN_primer_target_position"'),
+               h6('The position (well) of your samples in your labware (numeric values; column-wise ordering)'),
+               br(),
+               h4('Primers:'),
+               tags$ul(
+                 tags$li('If primer volume set to 0, then primers are skipped.')
+               ),
+               br(),
+               h4('Controls:'),
+               tags$ul(
+                 tags$li('For the positive & negative controls, include them in the mapping file'),
+                 tags$li('If the controls (or samples) are provided in a tube, include them in the mapping file and use either "1.5ml Eppendorf" or "2.0ml Eppendorf" for the labware type'),
+                 tags$li('See the example input table (under the "Example Input" tab)')
+               ),
+               h4('Labware:'),
+               tags$ul(
+                 tags$li('In order to use a plate adapter, use "PCR Adapter 96 Well and 96 Well Eppendorf TwinTec PCR"')
+               ),
+               br(),
+               h4('Output files:'),
+               h5('Worklist files and associated files will be created for the tagmentation assay setup and the PCR assay setup'),
+               h5('Tagmentation assay files = "*_tag_*"'),
+               h5('PCR assay files = "*_pcr_*"'),
+               h5('*_report.txt'),
+               h6('A summary of the PCR assay setup'),
+               h5('*_map.txt'),
+               h6('A mapping file with added PCR assay information'),
+               h5('*_labware.txt'),
+               h6('A table listing the labware to be placed on the robot worktable'),
+               h5('*.gwl'),
+               h6('A "worklist" file with instructions for the robot'),
+               h3('Notes'),
+               tags$ul(
+                 tags$li('All volumes are in ul'),
+                 tags$li('Plate well locations are 1 to n-wells; numbering by column')
+               )
+        )
+      )
+    ),
+    tabPanel('Input & Output',
+      fluidRow(
+        column(12,
+              h3('Input & output')
+        )
+      ),
+      fluidRow(
+        column(4,
+               h4('Input file'),
+               fileInput("MapFile", "A QIIME-formatted mapping file with extra columns (see Description)"),
+               textInput('rows',
+                            label = 'Which rows of the mapping file to use (eg., "all"=all rows; "1-48"=rows1-48; "1,3,5-6"=rows1+3+5+6)?',
+                            value = 'all')
+        ),
+        column(4,
+               h4('Output'),
+               textInput("prefix", 
+                         label = "Output file name prefix", 
+                         value = "TECAN_LITE")
+        )
+      )
+    ),
+    tabPanel("Destination Plate", 
+      fluidRow(
+        column(12, h4('Destination labware parameters'))
+      ),
+      fluidRow(
+        column(4,
+               textInput('destname',
+                         label = "Destination labware name",
+                         value = "Destination plate"),
+               selectInput('desttype',
+                           label = "Destination plate labware type (# of wells)",
+                           choices = c('96 Well Eppendorf TwinTec PCR' = '96 Well Eppendorf TwinTec PCR',
+                                       'PCR Adapter 96 Well and 96 Well Eppendorf TwinTec PCR' = 'PCR Adapter 96 Well and 96 Well Eppendorf TwinTec PCR',
+                                       '384 Well Biorad PCR' = '384 Well Biorad PCR'),
+                           selected = '384 Well Biorad PCR')
+        ),
+        column(4,
+               numericInput('deststart',
+                            label = "Start well number on destination plate",
+                            value = 1)
+        )
+      )
+    ),
+    tabPanel("Reagents", 
+      fluidRow(
+        column(12, h4('PCR reagent parameters'))
+      ),
+      fluidRow(
+        column(4,
+               h4("Total volumes"),
+               numericInput('tag_mm_volume',
+                            label = "Tagmentation MasterMix volume per reaction",
+                            value = 3.0),
+               numericInput('pcr_mm_volume',
+                            label = "PCR MasterMix volume per reaction",
+                            value = 10.0),
+               numericInput('sample_volume',
+                            label = "Sample volume per reaction",
+                            value = 2.0),
+               numericInput('primer_volume',
+                            label = "Primer volume (assuming primers are combined in 1 tube/well)",
+                            value = 10.0),
+               numericInput('errorperc',
+                            label = "% total volume to include in calculating total reagent needed",
+                            value = 10)
+        ),
+        column(4,
+               h4('Liquid classes'),
+               textInput('mm_liq',
+                         label = "Mastermix liquid class",
+                         value = "MasterMix Free Multi Wall Disp"),
+               textInput('sample_liq',
+                         label = "Sample liquid class",
+                         value = "Water Free Single Wall Disp"),
+               textInput('primer_liq',
+                         label = "Primer liquid class",
+                         value = "Water Free Single Wall Disp"),
+               numericInput('n_tip_reuse',
+                            label = "Number of tip reuses for multi-dispense",
+                            value = 6),
+               numericInput('n_multi_disp',
+                            label = "Number of multi-dispenses per tip (more multi-disp = more extra volume needed)",
+                            value = 4)
+        )
+      )
+    ),
+    tabPanel("Example Input",
+             fluidRow(
+               column(12, 
+                      h4('Mapping File format example'),
+                      h5('Note: the table can contain other columns')
+               )
+             ),
+             fluidRow(
+               column(12, DT::dataTableOutput('example_tbl'))
+             )
+    )
+  )
+))
