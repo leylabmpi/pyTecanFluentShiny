92c26941c01355b08d3a91f6b529fab622a1aa0d
Nick Youngblut <nyoungb2@gmail.com>
2018-12-12 17:25:46 +0100

    allowing for spaces in file name

--- utils/io.R
+++ utils/io.R
@@ -1,10 +1,13 @@
 # i/o utility functions 
 
-#' remaming temp file produced by fileInput (adding back file extension)
+#' Remaming temp file produced by fileInput (adding back file extension)
+#' Converting spaces to underscores (if present)
 #' select_input_obj = object generated from shiny::selectInput()
 rename_tmp_file = function(select_input_obj){
   file_ext = gsub('.+(\\.[^.])$', '\\1', select_input_obj$name) 
   new_file = paste0(select_input_obj$datapath, file_ext)
+  new_file = gsub(' +', '_', new_file)
   file.copy(select_input_obj$datapath, new_file, overwrite=TRUE)
   return(new_file)
 }
+
